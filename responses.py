85166a9f8e2e66aabc3f0ddc0af261faf2e33a42
iijonjon_ <42763271+Dr-CyNiKaL@users.noreply.github.com>
2023-05-18 02:16:35 -0400

    Delete responses.py

--- responses.py
+++
@@ -1,8 +0,0 @@
-def handle_response(message) -> str:
-    p_message = message.lower()
-
-    if p_message == 'hello':
-        return 'Hey there!'
-
-    if p_message == '!help':
-        return "`This is a help message that you can modify.`"
