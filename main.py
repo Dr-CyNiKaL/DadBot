505dfbae86c1f71d336edde1b0c6082214be94f5
iijonjon_ <42763271+Dr-CyNiKaL@users.noreply.github.com>
2023-05-18 14:06:34 -0400

    Added imhungry and music/vc bugs

--- main.py
+++ main.py
@@ -3,27 +3,27 @@ from nextcord.ext import commands
 import os
 from KEYS import *
 
-#-------------------------------------#
+#--------------------------------------------------------------------------------#
     ##########################
     #      TO-DO LIST        #
     ##########################
-    # 1. Music Capability? Need to check UMG TOS
-    # 2. Split files through COGs for easier management (v)
-    # 3. Roles for Social Credit System
-    # 4. Slash commands
-    # 5. Potential stored data for user games (far in the future)
+    # 1. Roles for Social Credit System
+    # 2. Slash commands
+    # 3. Potential stored data for user games (far in the future)
 
     ##########################
     #      KNOWN BUGS        #
     ##########################
     # 1. I'm hungry feature is still a little jank but it'll do
+    # 2. Music will not pause nor play
+    # 3. Player volume cannot be changed no matter what
 
     ###########################
     #      HELPFUL LINKS      #
     ###########################
     # 1. nextcord Documentation: https://docs.nextcord.dev/en/stable/api.html#
     # 2. HTML Color Picker: https://www.w3schools.com/colors/colors_picker.asp
-#-------------------------------------#
+#---------------------------------------------------------------------------------#
 
 version = "DadBot v0.1.0"
 intents = nextcord.Intents.all()
@@ -39,7 +39,7 @@ async def on_ready():
         print(f"- {guild.id} (name: {guild.name})")
         guild_count += 1
     
-    await client.change_presence(activity=nextcord.Game(name="catch with my son \U000026BE"))
+    await client.change_presence(activity=nextcord.Game(name="catch with my sons \U000026BE"))
 
     print("Dad is in " + str(guild_count) + " guilds.")
     print(f'{client.user} is now up and mowing the lawn! (%s)' % version)
@@ -51,8 +51,12 @@ for filename in os.listdir('./cogs'):
         initial_extensions.append(f'cogs.{filename[:-3]}')
 
 if __name__ == '__main__':
+    print('-----------------------------------------------------------')
+    print("Cogs Loaded: ")
     for extension in initial_extensions:
         client.load_extension(extension)
+        print(f'Loaded {extension}')
+    print('-----------------------------------------------------------')
 
 #print(initial_extensions)
 
